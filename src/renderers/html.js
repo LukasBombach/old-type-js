f4066c570a9932bc97218a5447d1eb9f9bdece88
Lukas Bombach <lukas.bombach@googlemail.com>
2015-04-19 14:22:45 +0200

    Setting up html renderer

--- src/renderers/html.js
+++ src/renderers/html.js
@@ -1,2 +1,15 @@
 'use strict';
 
+function renderNode(node) {
+  return node;
+}
+
+function HtmlRenderer(document) {
+  this.document = document;
+}
+
+HtmlRenderer.prototype.output = function () {
+  return renderNode(this.document);
+};
+
+module.exports = HtmlRenderer;
