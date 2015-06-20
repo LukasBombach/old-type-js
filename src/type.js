b03a0f1b6fe47264ad6375098043c38d6084e587
Lukas Bombach <lukas.bombach@googlemail.com>
2015-06-20 20:07:04 +0200

    Working on WYSIWYG formatting

--- src/type.js
+++ src/type.js
@@ -1,8 +1,15 @@
 'use strict';
 
 var Type = require('./core');
+
+// Load plugins
 require('./event');
 require('./cmd');
+
+// Expose Type
 window.Type = Type;
+
+// Dev
 window.Caret = require('./input/caret');
 window.Etherpad = require('./plugins/Etherpad/EtherpadLite');
+window.TypeRange = require('./type_rage');
