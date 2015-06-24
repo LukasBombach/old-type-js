ecc63d72faa4b249097a639f12cf77313e0293ef
Lukas Bombach <lukas.bombach@googlemail.com>
2015-06-24 23:57:45 +0200

    Working on software architecture

--- src/core_api.js
+++ src/core_api.js
@@ -5,9 +5,6 @@ var TypeRange = require('./type_range');
 
 (function () {
 
-  /**
-   * Todo this should only process parameters an call plugin methods
-   */
 
   /**
    *
@@ -16,79 +13,83 @@ var TypeRange = require('./type_range');
    */
   this.caret = function (params) {
 
-    // type.caret()
-    if (!params.length) {
-      return this._caret.absoluteOffset(); // todo was ist bei selektion?
+    // type.caret() todo was ist bei selektion?
+    if (!arguments.length) {
+      return this._caret.absoluteOffset();
     }
 
     // type.caret('show')
-    if (params[0] === 'show') {
+    if (arguments[0] === 'show') {
       this._caret.show();
       return this;
     }
 
     // type.caret('hide')
-    if (params[0] === 'hide') {
+    if (arguments[0] === 'hide') {
       this._caret.hide();
       return this;
     }
 
     // type.caret(10)
-    if (params.length === 1 && typeof params[0] === "number") {
-      this._caret.absoluteOffset(params[0]);
+    if (arguments.length === 1 && typeof arguments[0] === "number") {
+      this._caret.absoluteOffset(arguments[0]);
       return this;
     }
 
     // type.caret(10, 20)
-    if (params.length === 2) {
-      new TypeRange(this.root, params[0], params[1]).select();
+    if (arguments.length === 2) {
+      new TypeRange(this.root, arguments[0], arguments[1]).select();
       return this;
     }
 
   };
 
+  /**
+   *
+   * @param params
+   * @returns {*}
+   */
   this.selection = function (params) {
 
     // type.selection() || type.selection('text')
-    if (!params.length || (params.length === 1 && params[0] === 'text')) {
+    if (!arguments.length || (arguments.length === 1 && arguments[0] === 'text')) {
       return TypeRange.fromCurrentSelection().text();
     }
 
     // type.selection('html')
-    if (params[0] === 'html') {
+    if (arguments[0] === 'html') {
       return TypeRange.fromCurrentSelection().html();
     }
 
     // type.selection(10)
-    if (params.length === 1 && typeof params[0] === "number") {
-      this._caret.absoluteOffset(params[0]);
+    if (arguments.length === 1 && typeof arguments[0] === "number") {
+      this._caret.absoluteOffset(arguments[0]);
       return this;
     }
 
     // type.selection(10, 20)
-    if (params.length === 2) {
-      new TypeRange(this.root, params[0], params[1]).select();
+    if (arguments.length === 2) {
+      new TypeRange(this.root, arguments[0], arguments[1]).select();
       return this;
     }
 
     // type.selection(element)
-    if (DomUtil.isEl(params[0])) {
-      new TypeRange(params[0]).select();
+    if (DomUtil.isEl(arguments[0])) {
+      new TypeRange(arguments[0]).select();
       return this;
     }
 
     // type.selection(element1, element2)
-    if (params.length === 2 && DomUtil.isEl(params)) {
-      new TypeRange(params[0], params[1]).select();
+    if (arguments.length === 2 && DomUtil.isEl(arguments)) {
+      new TypeRange(arguments[0], arguments[1]).select();
       return this;
     }
 
     // type.selection(jQueryCollection) || type.selection([Array])
-    if (params[0].jQuery) {
-      new TypeRange(params[0], params[1]).select();
+    if (arguments[0].jQuery) {
+      new TypeRange(arguments[0], arguments[1]).select();
       return this;
     }
-
 
     // type.selection('save')
     // type.selection('restore', sel)
