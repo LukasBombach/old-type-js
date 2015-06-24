75a5922d7f96abb9d958837f9ae268c431dbdd63
Lukas Bombach <lukas.bombach@googlemail.com>
2015-06-24 20:12:34 +0200

    Working on software architecture

--- src/core_api.js
+++ src/core_api.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Type = require('./core');
+var TypeRange = require('./type_range');
 
 (function () {
 
@@ -8,7 +9,36 @@ var Type = require('./core');
    * Todo this should only process parameters an call plugin methods
    */
 
+  /**
+   * 
+   * @param params
+   * @returns {*}
+   */
   this.caret = function (params) {
+
+    if (!params.length) {
+      return this._caret.absoluteOffset();
+    }
+
+    if (params.length && params[0] === 'show') {
+      this._caret.show();
+      return this;
+    }
+
+    if (params.length && params[0] === 'hide') {
+      this._caret.hide();
+      return this;
+    }
+
+    if (params.length === 1 && typeof params[0] === "number") {
+      this._caret.absoluteOffset(params[0]);
+      return this;
+    }
+
+    if (params.length === 2) {
+      new TypeRange(this.root, params[0], params[1]).select();
+      return this;
+    }
 
   };
 
