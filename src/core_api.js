dacbd8786da701156ab3d8cd5cf2f65eca821188
Lukas Bombach <lukas.bombach@googlemail.com>
2015-06-24 19:38:44 +0200

    Working on software architecture

---
+++ src/core_api.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var Type = require('./core');
+
+(function () {
+
+  /**
+   * Todo this should only process parameters an call plugin methods
+   */
+
+  this.caret = function (params) {
+
+  };
+
+  this.selection = function (params) {
+
+  };
+
+  this.insert = function (params) {
+
+  };
+
+  this.format = function (params) {
+
+  };
+
+  this.remove = function (params) {
+
+  };
+
+  this.settings = function (params) {
+
+  };
+
+}).call(Type.fn);
---
+++ src/plugins/Etherpad/api.js
@@ -0,0 +1,9 @@
+'use strict';
+
+(function () {
+
+  /**
+   * Todo this should work like ../../core_api.js for this plugin
+   */
+
+}).call(Type.fn);
