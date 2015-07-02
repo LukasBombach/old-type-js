81fae02543aa4e5e1a0030414e00c4921b3efa7d
Lukas Bombach <luke@MacBook-Pro-Lukas.local>
2015-07-02 14:45:14 +0200

    Add documentation

--- src/type_selection.js
+++ src/type_selection.js
@@ -8,12 +8,15 @@ var TypeSelectionOverlay = require('./type_selection_overlay');
  * @constructor
  */
 function TypeSelection() {
-  this.unselect();
+  this._init();
 }
 
 (function () {
 
   /**
+   * Resets (removes) the current selection if there is one, sets a new anchor at
+   * the given coordinates and sets up a new selection at the node and offset found
+   * at the coordinates.
    *
    * @param {number} x - Absolute horizontal position on the document
    * @param {number} y - Absolute vertical position on the document
@@ -22,10 +25,14 @@ function TypeSelection() {
   this.beginAt = function (x, y) {
     this.unselect();
     this._setAnchor(x, y);
-    return this._beginNewAt(this._anchor.node, this._anchor.offset);
+    return this._startRangeAt(this._anchor.node, this._anchor.offset);
   };
 
   /**
+   * Will move the end or the start of the selection to the node and offset found at
+   * the given coordinates. Whether the start or the end will be moved depends on
+   * whether the coordinates are on top / left of this selection's anchor or below /
+   * right of it.
    *
    * @param {number} x - Absolute horizontal position on the document
    * @param {number} y - Absolute vertical position on the document
@@ -43,7 +50,7 @@ function TypeSelection() {
   };
 
   /**
-   * Removes all selection overlays and resets internal variables
+   * Removes all selection overlays and resets internal variables.
    *
    * @returns {TypeSelection} - This instance
    */
@@ -52,13 +59,14 @@ function TypeSelection() {
     this._elements = {};
     this._range = null;
     this._anchor = null;
-    this._start = null;
-    this._end = null;
     return this;
   };
 
   /**
-   * Returns whether or not this selection is visible
+   * Returns whether or not this selection is visible. By checking if there currently
+   * are any overlays and if the first overlay is actually visible. There should be
+   * no case where there are visible overlays but the first overlay wouldn't be visible,
+   * so this is a quick and performant way to check for the selection's visibility.
    *
    * @returns {boolean}
    */
@@ -67,12 +75,26 @@ function TypeSelection() {
   };
 
   /**
+   * Alias method for select() for better code readability. For initialization
+   * all variables should be set to their default values. This is what select
+   * does for us.
+   *
+   * @returns {TypeSelection} - This instance
+   * @private
+   */
+  this._init = function () {
+    return this.unselect();
+  };
+
+  /**
+   * Creates a new {Range}, which will be the basis for drawing and this selection.
+   * todo Use {TypeRange}? Should be cool if we don't use getRects or we make TypeRange more performant
    *
    * @param {Node} node - The text node that the selection should start in
    * @param {number} offset - The offset in the text node that the selection should start in
    * @returns {TypeSelection} - This instance
    */
-  this._beginNewAt = function (node, offset) {
+  this._startRangeAt = function (node, offset) {
     this._range = window.document.createRange();
     this._range.setStart(node, offset);
     this._range.setEnd(node, offset);
@@ -86,9 +108,6 @@ function TypeSelection() {
    * @returns {TypeSelection} - This instance
    */
   this._moveStartTo = function (node, offset) {
-    //if (node === this._range.startContainer && offset === this._range.startOffset) {
-    //  return this;
-    //}
     this._range.setStart(node, offset);
     this._range.setEnd(this._anchor.node, this._anchor.offset);
     this._imitateRangePrepending();
@@ -102,9 +121,6 @@ function TypeSelection() {
    * @returns {TypeSelection} - This instance
    */
   this._moveEndTo = function (node, offset) {
-    //if (node === this._range.endContainer && offset === this._range.endOffset) {
-    //  return this;
-    //}
     this._range.setStart(this._anchor.node, this._anchor.offset);
     this._range.setEnd(node, offset);
     this._imitateRangeAppending();
@@ -127,32 +143,6 @@ function TypeSelection() {
     var range = document.caretRangeFromPoint(x, y);
     this._anchor = {x: x, y: y, node: range.startContainer, offset: range.startOffset};
     this._addElement(this._anchor.node);
-    return this;
-  };
-
-  /**
-   * Sets the start node and offset for this selection
-   *
-   * @param {Node} node - A text node
-   * @param {number} offset - The character offset inside the text node
-   * @returns {TypeSelection} - This instance
-   * @private
-   */
-  this._setStart = function (node, offset) {
-    this._start = {node: node, offset: offset};
-    return this;
-  };
-
-  /**
-   * Sets the end node and offset for this selection
-   *
-   * @param {Node} node - A text node
-   * @param {number} offset - The character offset inside the text node
-   * @returns {TypeSelection} - This instance
-   * @private
-   */
-  this._setStart = function (node, offset) {
-    this._end = {node: node, offset: offset};
     return this;
   };
 
@@ -262,9 +252,7 @@ function TypeSelection() {
    */
   this._removeOverlays = function () {
     var i;
-    if (!this._overlays) {
-      this._overlays = [];
-    }
+    this._overlays = this._overlays || [];
     for (i = 0; i < this._overlays.length; i += 1) {
       this._overlays[i].remove();
     }
