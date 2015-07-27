117d4ba7e4b946f6c78a9e07dc23bd31b6c92a79
Lukas Bombach <lukas.bombach@googlemail.com>
2015-07-27 22:32:32 +0200

    Report bootcamp

--- src/development.js
+++ src/development.js
@@ -12,6 +12,19 @@ Type.Development = function () {
 (function () {
 
   /**
+   * Prints a message to the console if the browser's
+   * console offers the log method.
+   *
+   * @param {...*} messages - Any number and type of arguments
+   *     you want to pass to console.debug
+   */
+  Type.Development.debug = function (messages) {
+    if (console && console.log) {
+      console.log.apply(console, arguments)
+    }
+  };
+
+  /**
    * Prints a debug message to the console if the browser's
    * console offers a debug method.
    *
@@ -22,7 +35,7 @@ Type.Development = function () {
     if (console && console.debug) {
       console.debug.apply(console, arguments)
     }
-  }
+  };
 
 }).call(Type.Development);
 
