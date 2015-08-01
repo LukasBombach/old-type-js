3dd2a378e1b3ef8a832a9be925ab9d2d7b4d2e19
Lukas Bombach <lukas.bombach@googlemail.com>
2015-08-01 16:30:43 +0200

    Implemented OOP class

--- src/oop.js
+++ src/oop.js
@@ -1,5 +1,43 @@
-/**
- *
- *  Todo implement inherits method
- *
- */
+'use strict';
+
+var Type = require('./core');
+
+Type.OOP = function () {};
+
+(function () {
+
+  /**
+   * Implements classical inheritance for the constructor pattern
+   *
+   * @param {Function} constructor - The child class that shall
+   *     inherit attributes and methods
+   * @param {Function} parentConstructor - The parent class that
+   *     shall be inherited from
+   * @returns {Function} The child class that inherited
+   */
+  Type.OOP.inherits = function (constructor, parentConstructor) {
+
+    // Required variables
+    var key;
+
+    // Inherit instance attributes and methods
+    constructor.prototype = Object.create(parentConstructor.prototype);
+    constructor.prototype.constructor = constructor;
+
+    // Inherit static attributes and methods
+    for (key in parentConstructor) {
+      if (parentConstructor.hasOwnProperty(key))
+        constructor[key] = parentConstructor[key];
+    }
+
+    // Add parent / super property
+    constructor._super = parentConstructor;
+
+    // Return the inheriting class for convenience
+    return constructor;
+
+  };
+
+}).call(Type.OOP);
+
+module.exports = Type.OOP;
