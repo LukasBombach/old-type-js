8fdba2bd0543adf874b5c6d1490dd58f3eb7c847
Lukas Bombach <lukas.bombach@googlemail.com>
2015-08-25 17:20:43 +0200

    code fixes

--- src/core_api.js
+++ src/core_api.js
@@ -5,9 +5,24 @@ var Type = require('./core');
 (function () {
 
   /**
+   * Returns the offset of the caret
+   * type.caret()
    *
-   * @param params
-   * @returns {*}
+   * Show the caret
+   * type.caret('show')
+   *
+   * Hides the caret
+   * type.caret('hide')
+   *
+   * Moves the caret to the 10th character
+   * type.caret(10)
+   *
+   * Convenience function for type.select(10, 20)
+   * type.caret(10, 20)
+   *
+   * @param {...*} params - Various parameters are possible.
+   *     See examples in the block comment.
+   * @returns {Type} - The editor instance
    */
   this.caret = function (params) {
 
@@ -44,9 +59,39 @@ var Type = require('./core');
   };
 
   /**
+   * Same as type.selection('text')
+   * type.selection()
    *
-   * @param params
-   * @returns {*}
+   * Returns the unformatted (plain) contents of the current selection
+   * type.selection('text')
+   *
+   * Return the currently selected HTML
+   * type.selection('html')
+   *
+   * Convenience function for type.caret(10)
+   * type.selection(10)
+   *
+   * Selects characters 10 to 20
+   * type.selection(10, 20)
+   *
+   * Select an element
+   * type.selection(element)
+   *
+   * Creates a selection between 2 elements
+   * type.selection(element1, element2)
+   *
+   * Creates a selection between the first and last element in the jQuery Collection
+   * type.selection(jQueryCollection)
+   *
+   * Returns an object that can be passed to type.selection('restore') to store and recreate a selection
+   * type.selection('save')
+   *
+   * Takes an object returned by type.selection('save') as a second argument to recreate a stored selection
+   * type.selection('restore', sel)
+   *
+   * @param {...*} params - Various parameters are possible.
+   *     See examples in the block comment.
+   * @returns {Type} - The editor instance
    */
   this.selection = function (params) {
 
@@ -104,9 +149,22 @@ var Type = require('./core');
   };
 
   /**
+   * Inserts plain text at the caret's position, regardless if str contains html. Will overwrite the current
+   * type.insert(str)* selection if there is one.
    *
-   * @param params
-   * @returns {*}
+   *
+   * Inserts formatted text at the caret's position. Will overwrite the current selection if there is one.
+   * type.insert('html', str)
+   *
+   * Inserts str at the offset given as second parameter
+   * type.insert(str, 10)
+
+   * Same as type.insert(str, 10) but inserts formatted text given as html string
+   * type.insert('html', str, 10)
+   *
+   * @param {...*} params - Various parameters are possible.
+   *     See examples in the block comment.
+   * @returns {Type} - The editor instance
    */
   this.insert = function (params) {
 
@@ -140,9 +198,15 @@ var Type = require('./core');
   };
 
   /**
+   * Formats the currently selected text with the given tag.
+   * type.format(tagName, [...params])* E.g. use type.cmd('strong') to format the currently selected text bold
    *
-   * @param params
-   * @returns {*}
+   * Applies type.format to a specific text range
+   * type.format(startOffset, endOffset, tagName, [...params])
+   *
+   * @param {...*} params - Various parameters are possible.
+   *     See examples in the block comment.
+   * @returns {Type} - The editor instance
    */
   this.format = function (params) {
 
@@ -165,38 +229,54 @@ var Type = require('./core');
   };
 
   /**
+   * Deletes the currently selected text. Does nothing if there is no selection.
+   * type.remove()
    *
-   * @param params
-   * @returns {*}
+   * Removes a number of characters from the caret's position. A negative number will remove characters left
+   * of the caret, a positive number from the right. If there is a selection, the characters will be removed
+   * from the end of the selection.
+   * type.remove(numChars)
+   *
+   * Will remove characters between the given offsets
+   * type.remove(startOffset, endOffset)
+   *
+   * @param {...*} params - Various parameters are possible.
+   *     See examples in the block comment.
+   * @returns {Type} - The editor instance
    */
   this.remove = function (params) {
+
+    if (arguments.length < 2) {
+      this.getInput().getContent().remove(this.getCaret().getOffset(), arguments[0] || -1)
+    }
+
+    if (arguments.length === 2) {
+      this.getInput().getContent().remove(arguments[0], arguments[1]);
+    }
+
     return this;
   };
 
   /**
+   * Revokes the last user input
    *
-   * @param steps
-   * @returns {*}
+   * @param {Number} [steps] - The number of actions to revoke
+   * @returns {Type} - The editor instance
    */
   this.undo = function (steps) {
+    var sourceId = this.getInput().getContent().getSourceId();
+    this.getUndoManager().undo(sourceId, steps);
     return this;
   };
 
   /**
-   *
-   * @param steps
-   * @returns {*}
+   * Reapplies a revoked input
+   * @param {Number} [steps] - The number of actions to reapply
+   * @returns {Type} - The editor instance
    */
   this.redo = function (steps) {
-    return this;
-  };
-
-  /**
-   *
-   * @param params
-   * @returns {*}
-   */
-  this.settings = function (params) {
+    var sourceId = this.getInput().getContent().getSourceId();
+    this.getUndoManager().redo(sourceId, steps);
     return this;
   };
 
